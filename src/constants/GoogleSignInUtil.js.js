4713a00d15cf2426b06a19bc39d8fd456569a0e4
aaasoftwaresolution <aaasoftwaresolution01@gmail.com>
2023-12-11 03:41:21 +0530

    code working succesfully google login

--- src/constants/GoogleSignInUtil.js.js
+++ src/constants/GoogleSignInUtil.js.js
@@ -8,11 +8,11 @@ import {
   export const configureGoogleSignIn = () => {
     GoogleSignin.configure({
       webClientId:
-        "999204942642-l0p47r1tmv23ced7k36rd1e0c0f3p2dt.apps.googleusercontent.com",
+        "401912999040-8dksjba2jfpu6omjtq06idttjed4rvg5.apps.googleusercontent.com",
       androidClientId:
-        "401912999040-lbmmhon7cf0en86lk8lcj03cs51fds7l.apps.googleusercontent.com",
+        "401912999040-pgs6g9dgq8s77mv14qei0bk19li17lu3.apps.googleusercontent.com",
       iosClientId:
-        "999204942642-jfbbjt13l1s1jtv7m5n1cqtjuelluvr7.apps.googleusercontent.com",
+        "401912999040-96e6gf7oqull1hsh42qpejspci8noe7v.apps.googleusercontent.com",
     });
   };
   
